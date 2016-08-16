d8e51e5b384c51140b73f046db627220a2c5a05e
interisti <nika.interisti@gmail.com>
2016-08-16 23:48:07 +0400

    fix linter/builder

--- src/index.js
+++
@@ -1,9 +0,0 @@
-export class Point {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
-}
-if (require.main === module) {
-	let pt = new Point(7,4);
-}
--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -1,15 +1,19 @@
-var mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
-// Global before hook
-before(function (done) {
+/**
+ * Global before hook
+ */
+before(done => {
 	mongoose.connect('mongodb://localhost:27017/restDB', done);
 });
 
-// Global after hook
-after(function (done) {
-  // here you can clear fixtures, etc.
-  // remove database
-  mongoose.connection.db.dropDatabase();
+/**
+ * Global after hook
+ */
+after(done => {
+	// here you can clear fixtures, etc.
+	// remove database
+	mongoose.connection.db.dropDatabase();
 
-  done();
+	done();
 });
